a62dc7e479c9d785ae50711b9b2853edc2b14105
Tikai7 <krimo-metal11@hotmail.com>
2022-10-24 22:24:06 +0100

    v3

--- sort_heap.c
+++ sort_heap.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
+#include <math.h>
 
 void creer_tas(int tab[], int taille, int indice_racine)
 {
@@ -62,12 +64,109 @@ void tri_tas(int tab[], int tab_taille, int tab_middle)
     }
 }
 
+void tri_swap(int tab[], int taille)
+{
+    for (int i = 0; i < taille; i++)
+    {
+        for (int j = i; j < taille; j++)
+        {
+            if (tab[i] < tab[j])
+            {
+                int temp = tab[i];
+                tab[i] = tab[j];
+                tab[j] = temp;
+            }
+        }
+    }
+}
+
+void afficher_tab(int tab[], int taille)
+{
+    for (int i = 0; i < taille; i++)
+        printf("%d ", tab[i]);
+}
+
 int main()
 {
-    int tab[4] = {1, 4, 3, 2};
-    tri_tas(tab, 4, 2);
+    srand(17);
+    //-------------------------------- Variables
+    clock_t start, end;
 
-    for (int i = 0; i < 4; i++)
-        printf("tab[%d] = %d \n", i, tab[i]);
+    FILE *fichier = NULL;
+    double cpu_time_used;
+
+    int TAILLES[13] = {
+        5 * pow(10.0, 4.0),
+        1 * pow(10.0, 5.0),
+        2 * pow(10.0, 5.0),
+        4 * pow(10.0, 5.0),
+        8 * pow(10.0, 5.0),
+        3.2 * pow(10.0, 6.0),
+        6.4 * pow(10.0, 6.0),
+        12.8 * pow(10.0, 6.0),
+        25.6 * pow(10.0, 6.0),
+        51.2 * pow(10.0, 6.0),
+        1024 * pow(10.0, 6.0),
+        2048 * pow(10.0, 6.0),
+    };
+
+    //------------------------------------------------------- POUR TOUTE LES TAILLES
+    for (int i = 0; i < 13; i++)
+    {
+
+        //---------------------------------------------------- On crée un tableau de taille TAILLE[i]
+        int *tab = (int *)malloc(sizeof(int) * TAILLES[i]);
+        int size_tab = TAILLES[i];
+        int size_middle = (int)(TAILLES[i] / 2);
+
+        //---------------------------------------------------- REMPLISSAGE
+
+        for (int j = 0; j < TAILLES[i]; j++)
+            tab[j] = rand() % TAILLES[i];
+        //---------------------------------------------------- RANDOM VALUE
+        start = clock();
+        tri_tas(tab, size_tab, size_middle);
+        end = clock();
+        cpu_time_used = (double)(end - start) / CLOCKS_PER_SEC;
+
+        fichier = fopen("resultat_heap_random.txt", "a");
+        if (fichier)
+        {
+            fprintf(fichier, "Random values : %d  \n", cpu_time_used);
+            fclose(fichier);
+        }
+        //---------------------------------------------------- SORTED VALUE
+
+        tri_tas(tab, size_tab, size_middle);
+
+        start = clock();
+        tri_tas(tab, size_tab, size_middle);
+        end = clock();
+        cpu_time_used = (double)(end - start) / CLOCKS_PER_SEC;
+
+        fichier = fopen("resultat_heap_sorted.txt", "a");
+        if (fichier)
+        {
+            fprintf(fichier, "Sorted values : %d  \n", cpu_time_used);
+            fclose(fichier);
+        }
+
+        //---------------------------------------------------- SORTED VALUE (inverse)
+
+        tri_swap(tab, size_tab);
+
+        start = clock();
+        tri_tas(tab, size_tab, size_middle);
+        end = clock();
+        cpu_time_used = (double)(end - start) / CLOCKS_PER_SEC;
+
+        fichier = fopen("resultat_heap_sorted_inverse.txt", "a");
+        if (fichier)
+        {
+            fprintf(fichier, "Sorted Inverse values : %d  \n", cpu_time_used);
+            fclose(fichier);
+        }
+    }
+
     return 0;
 }
